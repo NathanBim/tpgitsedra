81ad52b3087e9a360d627a79ca678d90f0f43d98
Nathan <nathan.apb63@gmail.com>
2017-10-25 17:33:23 +0200

    diameter

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -6,10 +6,10 @@ class Sphere(object):
         self.radius = radius
         pass
 
-    # *** STEP 2 ***
-    # uncomment the 2 following lines
-    #def __str__(self):
-    #    return '%s(%s)' % (self.__class__.__name__, self.radius)
+    STEP 2
+    
+    def __str__(self):
+        return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
         return 4.0 * math.pi * self.radius ** 2
@@ -21,7 +21,9 @@ class Sphere(object):
         pass
 
     def diameter(self):
-        return self.radius
+        # *** STEP 5 ***
+        # uncomment the following line
+        #return self.radius
         pass
 
     def dump(self, filename):
